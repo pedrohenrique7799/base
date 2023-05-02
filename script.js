b80e7ed55e22da3e8aab389f9d3ad89d7665e313
pedrohenriquq7799 <phsn.sgorlon@gmail.com>
2023-05-02 16:49:03 -0400

    adicionando script

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // divListaProduto.insertAdjacentHTML('afterbegin', '');
 
+class Produto {
+    constructor(nome, datac, descricao, preco){
+            this.nome = nome;
+            this.datac = datac;
+            this.descricao = descricao;
+            this.preco = preco
+    }
+mostrap(){
+    this.nome + this.datac + this.descricao + this.preco
+}
+}
+class Produtod extends Produto{
+    constructor (nome , datac,descricao , preco, imagemd){
+        super(nome , datac , descricao, preco )
+this.imagemd = imagemd
+        
+    }
+
+mostrapd(){
+ return this.imagemd 
+}
+}
