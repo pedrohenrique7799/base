1cf9fb22cfd59cba744437794294f1ebe04a9ba1
pedrohenrique7799 <131817298+pedrohenrique7799@users.noreply.github.com>
2023-05-03 17:27:39 -0400

    adicionado produtos, arrumei o metodo mostrar produto, e alteraçoes esteticas e tecnicas no codigo

--- script.js
+++ script.js
@@ -3,22 +3,36 @@
 class Produto {
     constructor(nome, dataCadastro, descricao, valor){
             this.nome = nome;
-            this.dataCadastro = this.dataCadastro;
+            this.dataCadastro = dataCadastro;
             this.descricao = descricao;
             this.valor = valor
     }
 mostrarProduto(){
-    this.nome + this.dataCadastro + this.descricao + this.valor
+    return `
+    <h1>${this.nome}</h1>
+    <img src="${this.imagem}"
+    <div>${this.dataCadastro}</div>
+    <p>${this.descricao}</p>
+    `
+    //this.nome + this.dataCadastro + this.descricao + this.valor
 }
 }
-class Produto extends Produto{
-    constructor (nome , datac,descricao , preco, imagemd){
-        super(nome , datac , descricao, preco )
+class ProdutoDestaque extends Produto{
+    constructor (nome , dataCadastro,descricao , valor, imagem){
+        super(nome , dataCadastro , descricao, valor )
 this.imagem = imagem
         
     }
 
-mostrapd(){
- return this.imagemd 
+mostrarProdutoDestq(){
+ return this.imagem
 }
 }
+
+const produto = new Produto("Liquidificador", "17/09/2022", "Liquidificador Mundial power3x", 129,90)
+const produtoDestaque = new ProdutoDestaque("Faca", "12/02/2023", "Faca Tramontina Corte Rápido", 259,99, "https://images.tcdn.com.br/img/img_prod/959075/faca_chef_tramontina_plenus_lamina_em_aco_inox_cabo_preto_8_299_1_c8dc136d540307cf96cd4a52b6c843a1.jpg")
+console.log(produto.mostrarProduto())
+console.log(produtoDestaque.mostrarProdutoDestq())
+
+const produtoPadrao = document.getElementById("lista-produtos")
+produtoDestaque.insertAdjacentHTML('afterbegin', produto.mostrarProduto)
